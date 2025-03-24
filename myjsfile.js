514662066d4ca53406931a9d54fa876da63c808d
qasemjr7 <elonmuskjr666@gmail.com>
2025-03-24 09:11:06 -0700

    Update myjsfile.js

--- myjsfile.js
+++ myjsfile.js
@@ -1,69 +1,28 @@
-
-
-
- numbers = [ ]
-
-for ( i = 0; i < 10; i++) {
- input = prompt("یه عدد بین 1 تا 99 وارد کنید " );
-        numbers.push(input);
-}
-maxNumber = numbers[0]
-
-for ( i = 0; i < 10; i++) {
-    if (numbers[i] > maxNumber) {
-        maxNumber = numbers[i]
-    }
-}
-
-
-document.getElementById("test").innerHTML = `<h1>بزرگترین عدد=${maxNumber}</h1>`
-
-day = new Date ()
-time = day.getDay()
-console.log(time);
-
-
-  if(time==6){
-    document.getElementById("date").innerHTML = "<p> <big><big>شنبه</big></big></p>   "    
-    }
-    else if(time==0){
-        document.getElementById("date").innerHTML = "<p  <big><big>یکشنبه</big></big></p>   " 
-        }
-       else if(time==1){
-            document.getElementById("date").innerHTML = "<p> <big><big>دوشنبه</big></big></p>   " 
-            }
-          else  if(time==2){
-                document.getElementById("date").innerHTML = "<p> <big><big>سه شنبه</big></big></p>   "  
-                }
-             else   if(time==3){
-                    document.getElementById("date").innerHTML = "<p> <big><big>چهارشنبه</big></big></p>   "     
-                    }
-                  else  if(time==4){
-                        document.getElementById("date").innerHTML = "<p> <big><big>پنجشنبه</big></big></p>   "    
-                        }
-                        else if (time==5){
-                            document.getElementById("date").innerHTML = "<p> <big><big>جمعه</big></big></p> "
-                        }
-                        
-getElement = document.getElementById("clock")
-clock = new Date()
-hours = clock.getHours();
-console.log(time);
-if (hours <= 7){
-getElement.innerHTML= "بامداد"
-}
-else if (hours <= 12){
-  getElement.innerHTML= "صبح"
-}
-else if (hours <= 16){
-  getElement.innerHTML= "ظهر"
-  }
-  else if (hours <= 19){
-    getElement.innerHTML= "بعد ظهر "
-    }
-    else{
-      getElement.innerHTML= "شب"
+function Game (){
+    randomNumber= ( 1 + Math.round( Math.random ()*99) )
+    window.alert("امزوش بازی :شما 3 شانش برای پیدا کردن یک عدد شانسی داری و بعد از هر بار اشتباه یک راهنمای به شما داده میشود ")
+  let score = 30
+    for (let i = 0; i < 3; i++) {
+     const input = window.prompt("یک عدد بین 1 تا 100 وارد  کنید ")
+      if(randomNumber==input){
+       document.getElementById("Winner").classList.add("winner")
+       break
       }
-      getelement = document.getElementById("Hours").innerHTML = ` ساعت:${hours}`
+
+ else if (randomNumber>input){
+    window.alert("شما عدد کوچکتر تر را وارد کرده اید ")
+   score = score - 10
+}
+ else if (randomNumber < input){
+    window.alert("شما عدد بزرگتر را وارد کرده اید")
+    score = score - 10
+}
+}
+
+ if(score == 0) {
+    document.getElementById("A").classList.add("Losser")
+}
+}
+
 
 
